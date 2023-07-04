38531250ea6d173f5b4e31b147ef4a4cc4d33955
andjv <avesic76@gmail.com>
2023-07-04 23:11:33 +0200

    Zaposleni API

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -24,6 +24,11 @@ Route::get('check-login/{id}', [EvidencijaKontroler::class, 'checkLogin']);
 Route::get('prisustva', [EvidencijaKontroler::class, 'prisustva']);
 Route::get('kasnjenja', [EvidencijaKontroler::class, 'kasnjenja']);
 
+Route::get('zaposleni', [ZaposleniKontroler::class, 'zaposleni']);
+Route::get('zaposleni-search/{input}', [ZaposleniKontroler::class, 'zaposleniSearch']);
+Route::get('zaposleni-sort/{sortiranje}', [ZaposleniKontroler::class, 'zaposleniSort']);
+Route::delete('zaposleni-delete/{idZaposleni}', [ZaposleniKontroler::class, 'zaposleniDelete']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
