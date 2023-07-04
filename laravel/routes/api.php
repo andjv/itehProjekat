45acd72a03cd078531784401857843c108c77532
andjv <avesic76@gmail.com>
2023-07-04 22:37:20 +0200

    Evidencija API

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -20,6 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthKontroler::class, 'register']);
 Route::post('login', [AuthKontroler::class, 'login']);
 
+Route::get('check-login/{id}', [EvidencijaKontroler::class, 'checkLogin']);
+Route::get('prisustva', [EvidencijaKontroler::class, 'prisustva']);
+Route::get('kasnjenja', [EvidencijaKontroler::class, 'kasnjenja']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
